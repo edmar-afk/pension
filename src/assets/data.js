f1f963fc999487d7d88b8298d3e0467e574b7329
project2479 <jaywrsnp6@gmail.com>
2024-09-12 11:13:18 +0800

    new

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,12 +1,12 @@
 const questions = [
 	{ id: 1, question: "When do you open?" },
-	{ id: 2, question: "What's the WiFi Password?" },
-	{ id: 3, question: "What's the WiFi Password 1?" },
-	{ id: 4, question: "What's the WiFi Password 2?" },
-	{ id: 5, question: "What's the WiFi Password 3?" },
-	{ id: 6, question: "What's the WiFi Password 4?" },
-	{ id: 7, question: "What's the WiFi Password 5?" },
-	{ id: 8, question: "What's the WiFi Password 6?" },
+	{ id: 2, question: "Hi?" },
+	{ id: 3, question: "Good Morning" },
+	{ id: 4, question: "I need help of something?" },
+	{ id: 5, question: "Can you help me?" },
+	{ id: 6, question: "Bye" },
+	{ id: 7, question: "Cool" },
+	{ id: 8, question: "Ok" },
 ];
 
 export default questions;
